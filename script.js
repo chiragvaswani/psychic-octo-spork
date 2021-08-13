c3364d78e9797b162750957d8549674ad29736a9
chiragvaswani <chiragvaswani751@gmail.com>
2021-08-13 13:27:40 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,48 @@
+$(function () {
+  window.sr = ScrollReveal();
+  console.log("Was here");
+  if ($(window).width() < 768) {
+    console.log("HEllo");
+    if ($(".timeline-content").hasClass("js--fadeInLeft")) {
+      $(".timeline-content")
+        .removeClass("js--fadeInLeft")
+        .addClass("js--fadeInRight");
+    }
+
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+  } else {
+    console.log($(body).width());
+    sr.reveal(".js--fadeInLeft", {
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+  }
+
+  sr.reveal(".js--fadeInLeft", {
+    origin: "left",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
+
+  sr.reveal(".js--fadeInRight", {
+    origin: "right",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
+});
